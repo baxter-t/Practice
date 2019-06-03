8e2179a1622868c60b2323c4254daf66739fc653
baxter-t <baxter.t98@gmail.com>
2019-06-03 13:58:30 +1000

    More

---
+++ findUnique.py
@@ -0,0 +1,8 @@
+def find_uniq(arr) :
+	first = arr[0]
+	matching = [x for x in arr if x != first]
+	return matching[0] if len(matching) == 1 else first
+
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+print(find_uniq([ 0, 0, 0.55, 0, 0 ]))
+print(find_uniq([ 3, 10, 3, 3, 3 ]))
