d6b128d208c4d5fa8eb48184fe2127dd5e4ca86e
baxter-t <baxter.t98@gmail.com>
2019-06-24 08:34:43 +1000

    Daily Practice

---
+++ maxSubArray.py
@@ -0,0 +1,13 @@
+'''
+	Given an array of integers and a number k, compute the maximum values of each
+	subarray of length k
+'''
+
+def maxSub(arr, k):
+	sub = []
+	for i in range(len(arr) - k + 1):
+		sub = arr[i : i  + k]
+		if len(sub) == k:
+			print(max(sub))
+
+maxSub([10, 5, 2, 7, 8, 7], 3)
