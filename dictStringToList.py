e93825b094ffb1aaf6ad366bd88e52e2d195cc1c
baxter-t <baxter.t98@gmail.com>
2019-06-27 07:56:36 +1000

    Daily Practice

---
+++ dictStringToList.py
@@ -0,0 +1,20 @@
+'''
+	Given a dictionary of words and a string of those words with no spaces
+	return a list that is the original sentence
+'''
+
+def toStng(dictionary, strng):
+	index = 0
+	result = []
+	while len(strng) > 0:
+		if strng[:index] in dictionary:
+			result.append(strng[:index])
+			strng = strng[index:]
+			index = 0
+		index += 1
+	return result
+
+strngTest = "thequickbrownfox"
+dicTest = ['quick', 'brown', 'the', 'fox']
+
+print(toStng(dicTest, strngTest))
