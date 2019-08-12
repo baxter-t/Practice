2a604d4df1905b6e48161a49197f2f0c0d2ac7f7
baxter-t <baxter.t98@gmail.com>
2019-08-12 15:34:29 +1000

    Working on the linkedlist Z

--- cPractice/linkedList.c
+++ cPractice/linkedList.c
@@ -13,13 +13,45 @@ struct Node {
 
 struct LinkedList {
 	struct Node *root;
+	struct Node *tail;
 };
+
+void 
+add_LL(struct LinkedList list, int value)
+{
+    struct Node new = {
+	.value = value,
+	.next = NULL,
+    };
+
+    if (list.root == NULL) {
+	list.root = &new;
+	list.tail = &new;
+
+	return;
+    }
+
+    list.tail->next = &new;
+    list.tail = &new;
+}
+
 
 int main(int argc, char **argv) {
 	struct LinkedList list;
-	struct Node first = {
-		.value = 1;
-	}
+    add_LL(list, 1);
+    add_LL(list, 2);
+    add_LL(list, 3);
+    add_LL(list, 4);
+    add_LL(list, 5);
 
+    struct Node *current = list.root;
+
+    while (current != NULL) {
+	printf("Value: %d\n", current->value);
+	current = current->next;
+    }
+
+
+	
 	
 }
